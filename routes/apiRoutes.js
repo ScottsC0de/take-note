6a72da250544f14e59878b0401b8ea8c72df7893
scottsc0de <scott5902@gmail.com>
2022-12-06 11:51:22 -0500

    working get/push methods

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 // import and initiate express.js by storing it in an "app" variable
 const express = require('express')
 const app = express();
+const uuid = require('uuid');
 
 // import db file
 const data = require('../db/db.json');
@@ -11,22 +12,27 @@ const path = require('path');
 
 // get() method
 app.get('/notes', (req, res) => {
-    res.json(data) // then grab our notes in JSON format
-    console.log("We have received your note!") // success message in log
+    res.json(data); // then grab our notes in JSON format
 });
 
 // post() method
 app.post('/notes', (req, res) => {
+    req.body.id = uuid.v4();
     data.push(req.body);
     res.json(data);
     fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(data), (err) =>
-        err ? console.log(err) : console.log("Your note has been saved!"))
+        err ? console.log(err) : console.log("Your note has been saved!"));
 });
 
 // delete() method
-// app.delete('public/notes/id', (req, res) => {
-//     // data.splice(req.body);
-//     // console.log("Your note has been deleted!")
-// });
+app.delete('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    const filteredNoteArray = data.filter((note) => {
+        return note.id !== noteId;
+    });
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(filteredNoteArray))
+    console.log("Your note was deleted!")
+    res.sendStatus(200);
+});
 
 module.exports = app;
